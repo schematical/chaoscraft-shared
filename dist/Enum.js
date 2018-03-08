946acabf593a6dfa922742078ef0255a6bfa9096
Matt <mlea@schematical.com>
2018-03-08 13:57:01 -0600

    Setting up

--- dist/Enum.js
+++ dist/Enum.js
@@ -22,16 +22,54 @@ class EnumOutputTypes {
         this.equip = 'equip';
         this.attack = 'attack';
         this.activateItem = 'activateItem';
+        this.deactivateItem = 'deactivateItem';
+        this.walkLeft = 'walkLeft';
+        this.walkRight = 'walkRight';
+        this.jump = 'jump';
+        this.sneak = 'sneak';
+        this.sprint = 'sprint';
+        this.clearControlStates = 'clearControlStates';
+        this.lookLeft = 'lookLeft';
+        this.lookRight = 'lookRight';
+        this.lookUp = 'lookUp';
+        this.lookDown = 'lookDown';
+        this.toss = 'toss';
+        this.activateBlock = 'activateBlock';
+        this.activateEntity = 'activateEntity';
+        this.useOn = 'useOn';
+        this.craft = 'craft';
+        this.openChest = 'openChest';
+        this.openFurnace = 'openFurnace';
+        this.openDispenser = 'openDispenser';
+        this.openEnchantmentTable = 'openEnchantmentTable';
+        this.openVillager = 'openVillager';
+        this.trade = 'trade';
+        this.openEntity = 'openEntity';
     }
 }
 class EnumInputTypes {
     constructor() {
+        this.health = 'health';
         this.canDigBlock = 'canDigBlock';
         this.hasInInventory = 'hasInInventory';
         this.canSeeEntity = 'canSeeEntity';
         this.canSeeBlock = 'canSeeBlock';
         this.chat = 'chat';
         this.onCorrelateAttack = 'onCorrelateAttack';
+        this.rain = 'rain';
+        this.entityMoved = 'entityMoved';
+        this.entitySwingArm = 'entitySwingArm';
+        this.entitySpawn = 'entitySpawn';
+        this.playerCollect = 'playerCollect';
+        this.entityUpdate = 'entityUpdate';
+        this.blockUpdate = 'blockUpdate';
+        this.diggingCompleted = 'diggingCompleted';
+        this.diggingAborted = 'diggingAborted';
+        this.move = 'move';
+        this.forcedMove = 'forcedMove';
+        this.chestLidMove = 'chestLidMove';
+        this.blockBreakProgressEnd = 'blockBreakProgressEnd';
+        this.blockBreakProgressObserved = 'blockBreakProgressObserved';
     }
 }
 //# sourceMappingURL=Enum.js.map
--- src/Enum.ts
+++ src/Enum.ts
@@ -19,13 +19,55 @@ class EnumOutputTypes{
     equip:string = 'equip';
     attack:string = 'attack';
     activateItem:string = 'activateItem';
+    deactivateItem:string = 'deactivateItem';
+    walkLeft:string = 'walkLeft';
+    walkRight:string = 'walkRight';
+    jump:string = 'jump';
+    sneak:string = 'sneak';
+    sprint:string = 'sprint';
+    clearControlStates:string = 'clearControlStates';
+    lookLeft:string = 'lookLeft';
+    lookRight:string = 'lookRight';
+    lookUp:string = 'lookUp';
+    lookDown:string = 'lookDown';
+    toss:string = 'toss';
+    activateBlock:string = 'activateBlock';
+    activateEntity:string = 'activateEntity';
+    useOn:string = 'useOn';
+    craft:string = 'craft';
+    openChest:string = 'openChest';
+    openFurnace:string = 'openFurnace';
+    openDispenser:string = 'openDispenser';
+    openEnchantmentTable:string = 'openEnchantmentTable';
+    openVillager:string = 'openVillager';
+    trade:string = 'trade';
+    openEntity:string = 'openEntity';
+
+
+
+
 }
 class EnumInputTypes{
+    health:string = 'health';
     canDigBlock:string = 'canDigBlock';
     hasInInventory:string = 'hasInInventory';
     canSeeEntity:string = 'canSeeEntity';
     canSeeBlock:string = 'canSeeBlock';
     chat:string = 'chat';
     onCorrelateAttack:string = 'onCorrelateAttack';
+    rain:string = 'rain';
+    entityMoved:string = 'entityMoved';
+    entitySwingArm:string ='entitySwingArm';
+    entitySpawn:string = 'entitySpawn';
+    playerCollect:string = 'playerCollect';
+    entityUpdate:string = 'entityUpdate';
+    blockUpdate:string = 'blockUpdate';
+    diggingCompleted:string = 'diggingCompleted';
+    diggingAborted:string = 'diggingAborted';
+    move:string = 'move';
+    forcedMove:string = 'forcedMove';
+    chestLidMove:string = 'chestLidMove';
+    blockBreakProgressEnd:string = 'blockBreakProgressEnd';
+    blockBreakProgressObserved:string = 'blockBreakProgressObserved';
 }
 export { Enum }
